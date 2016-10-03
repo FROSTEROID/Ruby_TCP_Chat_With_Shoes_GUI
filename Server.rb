cc6226b3222f72c156f3f27ab791a7656540f84a
FROSTEROID <frosteroidous@gmail.com>
2016-10-03 22:18:33 +0500

    Implemented the essentials on the server side. Seems to be working! :D

--- Server.rb
+++ Server.rb
@@ -4,14 +4,18 @@ require 'socket'
 $PORT_SERVICE = 5599
 $PORT_FIRST= 5600
 $CLIENTS_COUNT_MAX = 10
+
+#0-close; 1-open; 2-connected;
 $clientsState = []
 1.upto($CLIENTS_COUNT_MAX){$clientsState.push(0)}
 
+$SessionSocketThreads = []
+1.upto($CLIENTS_COUNT_MAX){$SessionSocketThreads.push(nil)}
+
+$SessionSockets = []
+1.upto($CLIENTS_COUNT_MAX){$SessionSockets.push(nil)}
 
  Shoes.app :width => 600, :height => 500 do
- 
-	#@el_address = edit_line "10"
-	#	@el_address.style :width=>512
 	@btn_listen = button "Listen"
 	@eb_dialog = edit_box
 		@eb_dialog.style :width=>600, :height=>400
@@ -19,37 +23,84 @@ $clientsState = []
 		@el_msg.style :width=>600
 	@btn_tst = button "test"
 	
-	@btn_listen.click {
+	@btn_listen.click proc{
 		serviceSocketThread = Thread.new{
-			@eb_dialog.text += "###### STARTED LISTENING ON " + $PORT_SERVICE.to_s + " TCP ######\n"
-			_socket_server = TCPServer.new $PORT_SERVICE
+			ServiceSocketThreadProc()
+		}
+	}
+	
+	# The procedure of TCP server on PORT_SERVICE.
+	def ServiceSocketThreadProc ()
+		@socket_server = TCPServer.new $PORT_SERVICE
+		@eb_dialog.text += "##### STARTED LISTENING ON " + $PORT_SERVICE.to_s + " TCP #####\n"
 			loop do
-				_socket = _socket_server.accept    # Wait for a client to connect
-				@clientNumber = 0				
+				_socket = @socket_server.accept    # Wait for a client to connect
+				@clientNumber = 0			
 				while @clientNumber < $CLIENTS_COUNT_MAX do
-					if clientsState[@clientNumber] == 0
-						clientsState[@clientNumber] = 1
+					if $clientsState[@clientNumber] == 0 then
+						$clientsState[@clientNumber] = 1
 						break
 					end
 					@clientNumber += 1
 				end
-				if @clientNumber < CLIENTS_COUNT_MAX
-					@eb_dialog.text += "##### GOT A CLIENT. ASSIGNED PORT IS " + (PORT_FIRST + @clientNumber).to_s + "#####\n"
-					_socket.puts (PORT_FIRST + @clientNumber).to_s
+
+				if @clientNumber < $CLIENTS_COUNT_MAX
+					@eb_dialog.text += "##### GOT A CLIENT FROM "+_socket.peeraddr().to_s+". ASSIGNED PORT IS " + ($PORT_FIRST + @clientNumber).to_s + ". #####\n"
+					_socket.puts ($PORT_FIRST + @clientNumber).to_s
+					Thread.new {
+						SessionSocketThreadProc(@clientNumber)
+					}
 				else
-					@eb_dialog.text += "##### GOT A CLIENT. NO SLOTS AVAILABLE. #####\n"
+					@eb_dialog.text += "##### GOT A CLIENT FROM "+_socket.peeraddr[:numeric]+". ASSIGNED PORT IS " + ($PORT_FIRST + @clientNumber).to_s + ". NO SLOTS AVAILABLE! #####\n"
 					_socket.puts "0"
 				end
 				_socket.close
+		end
+	end	
+	
+	#The procedure that is ran for every connected client. Uses ports from PORT_FIRST to PORT_FIRST + CLIENTS_COUNT_MAX - 1
+	def SessionSocketThreadProc (clientNumber)
+		info("lal! " + clientNumber.to_s + "!\n")
+		@eb_dialog.text += "##### Opened the port for the client. #####\n"
+		# Create a server for listening
+		@socket_session_server = TCPServer.new $PORT_FIRST + clientNumber	
+		# Wait for a client to connect
+		$SessionSockets[clientNumber] = @socket_session_server.accept    	
+		# Mark the client number as connected
+		$clientsState[clientNumber] = 2								
+		# Log the connection event
+		@eb_dialog.text += "##### A CLIENT CONNECTED TO " + ($PORT_FIRST + clientNumber).to_s + ". #####\n"
+		# Await for msgs to come and die
+		while msgFromClient = $SessionSockets[clientNumber].gets 	# Read lines from socket
+			# Output the message along with the client number
+			@eb_dialog.text += "("+clientNumber.to_s+") "+msgFromClient
+			BroadcastMessage(msgFromClient, clientNumber)
+		end
+		# No moar messages - the connections died. Mark the client number as close
+		$clientsState[clientNumber] = 0
+		# Aaaaand close the socket
+		$SessionSockets[clientNumber].close()
+		return nil
+	end
+	
+	# This will send the message to every client excepting the sourceClient
+	def BroadcastMessage(message, sourceClient)
+		@curClientID = 0
+		$SessionSockets.each do |curSock|
+			if(@curClientID != sourceClient && $clientsState[@curClientID] == 2)
+				curSock.puts "(#{sourceClient}) #{message}"
 			end
-		}
-	}
+			@curClientID += 1
+		end
+	end
+
 	
 	@btn_tst.click {
-		this.width = 888
+		
 	}
 
 	@el_msg.finish = proc {
+		BroadcastMessage(@el_msg.text, -1)
 		@eb_dialog.text = @eb_dialog.text + @el_msg.text + "\n"
 		@el_msg.text = ""
 	}
