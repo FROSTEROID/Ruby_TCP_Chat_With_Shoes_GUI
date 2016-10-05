c715ae2e50a46eee8ca63b9b084248b00d7bf238
FROSTEROID <frosteroidous@gmail.com>
2016-10-06 00:42:20 +0500

    Now the new lines come to the top. Because Shoes can't autoscroll. Or i'm just too stupid to figure out how to do it.

--- Server.rb
+++ Server.rb
@@ -32,7 +32,7 @@ $SessionSockets = []
 	# The procedure of TCP server on PORT_SERVICE.
 	def ServiceSocketThreadProc ()
 		@socket_server = TCPServer.new $PORT_SERVICE
-		@eb_dialog.text += "##### STARTED LISTENING ON " + $PORT_SERVICE.to_s + " TCP #####\n"
+		@eb_dialog.text = "##### STARTED LISTENING ON " + $PORT_SERVICE.to_s + " TCP #####\n" + @eb_dialog.text
 			loop do
 				_socket = @socket_server.accept    # Wait for a client to connect
 				@clientNumber = 0			
@@ -45,13 +45,13 @@ $SessionSockets = []
 				end
 
 				if @clientNumber < $CLIENTS_COUNT_MAX
-					@eb_dialog.text += "##### GOT A CLIENT FROM "+_socket.peeraddr().to_s+". ASSIGNED PORT IS " + ($PORT_FIRST + @clientNumber).to_s + ". #####\n"
+					@eb_dialog.text = "##### GOT A CLIENT FROM "+_socket.peeraddr().to_s+". ASSIGNED PORT IS " + ($PORT_FIRST + @clientNumber).to_s + ". #####\n" + @eb_dialog.text
 					_socket.puts ($PORT_FIRST + @clientNumber).to_s
 					Thread.new {
 						SessionSocketThreadProc(@clientNumber)
 					}
 				else
-					@eb_dialog.text += "##### GOT A CLIENT FROM "+_socket.peeraddr[:numeric]+". ASSIGNED PORT IS " + ($PORT_FIRST + @clientNumber).to_s + ". NO SLOTS AVAILABLE! #####\n"
+					@eb_dialog.text = "##### GOT A CLIENT FROM "+_socket.peeraddr[:numeric]+". ASSIGNED PORT IS " + ($PORT_FIRST + @clientNumber).to_s + ". NO SLOTS AVAILABLE! #####\n" + @eb_dialog.text
 					_socket.puts "0"
 				end
 				_socket.close
@@ -61,7 +61,7 @@ $SessionSockets = []
 	#The procedure that is ran for every connected client. Uses ports from PORT_FIRST to PORT_FIRST + CLIENTS_COUNT_MAX - 1
 	def SessionSocketThreadProc (clientNumber)
 		info("lal! " + clientNumber.to_s + "!\n")
-		@eb_dialog.text += "##### Opened the port for the client. #####\n"
+		@eb_dialog.text = "##### Opened the port for the client. #####\n" + @eb_dialog.text
 		# Create a server for listening
 		@socket_session_server = TCPServer.new $PORT_FIRST + clientNumber	
 		# Wait for a client to connect
@@ -69,11 +69,11 @@ $SessionSockets = []
 		# Mark the client number as connected
 		$clientsState[clientNumber] = 2								
 		# Log the connection event
-		@eb_dialog.text += "##### A CLIENT CONNECTED TO " + ($PORT_FIRST + clientNumber).to_s + ". #####\n"
+		@eb_dialog.text = "##### A CLIENT CONNECTED TO " + ($PORT_FIRST + clientNumber).to_s + ". #####\n" + @eb_dialog.text
 		# Await for msgs to come and die
 		while msgFromClient = $SessionSockets[clientNumber].gets 	# Read lines from socket
 			# Output the message along with the client number
-			@eb_dialog.text += "("+clientNumber.to_s+") "+msgFromClient
+			@eb_dialog.text = "("+clientNumber.to_s+") "+msgFromClient + @eb_dialog.text
 			BroadcastMessage(msgFromClient, clientNumber)
 		end
 		# No moar messages - the connections died. Mark the client number as close
@@ -101,7 +101,7 @@ $SessionSockets = []
 
 	@el_msg.finish = proc {
 		BroadcastMessage(@el_msg.text, -1)
-		@eb_dialog.text = @eb_dialog.text + @el_msg.text + "\n"
+		@eb_dialog.text = "(-1) " + @el_msg.text + "\n" + @eb_dialog.text
 		@el_msg.text = ""
 	}
  end
